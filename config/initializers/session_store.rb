d451fe5e625dd58194570c4c56e6bf354bc50bfe
Vinh Nguyen <xuanvinh@live.com>
2014-03-28 05:38:04 +0700

    use cache store cookies

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Znote::Application.config.session_store :cookie_store, key: '_znote_session'
+Znote::Application.config.session_store :cache_store, key: '_znote_session'
