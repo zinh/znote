18842b1d7bba2e33c51034bf3b0cf43db65c4a88
Vinh Nguyen <vinhnx@zigexn.vn>
2014-03-28 10:49:56 +0700

    add content_html column

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140304081926) do
+ActiveRecord::Schema.define(version: 20140328034540) do
 
   create_table "notes", force: true do |t|
-    t.integer  "user_id",    null: false
+    t.integer  "user_id",      null: false
     t.string   "title"
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "content_html"
   end
 
   add_index "notes", ["user_id"], name: "index_notes_on_user_id", using: :btree
