42739aa869d008d9a32e8f90f042aa33cb7d080c
Vinh Nguyen <vinhnx@zigexn.vn>
2014-03-31 09:41:00 +0700

    use psql

--- Gemfile
+++ Gemfile
@@ -4,7 +4,9 @@ source 'https://rubygems.org'
 gem 'rails', '4.0.3'
 
 # Use mysql as the database for Active Record
-gem 'mysql2'
+# gem 'mysql2'
+
+gem 'pg'
 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140328034540) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "notes", force: true do |t|
     t.integer  "user_id",      null: false
     t.string   "title"
